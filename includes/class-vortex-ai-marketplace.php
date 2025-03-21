3144ddd16fba7ae23a57be837e584754a942434e
nehalayub <nehalayub233@gmail.com>
2025-03-21 12:59:23 +0500

    fix: calling correct method

--- includes/class-vortex-ai-marketplace.php
+++ includes/class-vortex-ai-marketplace.php
@@ -167,7 +167,7 @@ class Vortex_AI_Marketplace {
 
         $this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_styles' );
         $this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_scripts' );
-        $this->loader->add_action( 'admin_menu', $plugin_admin, 'add_plugin_admin_menu' );
+        $this->loader->add_action( 'admin_menu', $plugin_admin, 'register_admin_menu' );
     }
 
     /**
